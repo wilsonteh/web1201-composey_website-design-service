3296daa0a5ac81451e24d7d7870f5661962c5100
Wilson Teh <ywteh13@gmail.com>
2021-11-16 10:56:15 +0800

    Change normal variable to heroku config var in order to connect to heroku with local phpMyAdmin

--- php/db_connection.php
+++ php/db_connection.php
@@ -2,12 +2,18 @@
 
 if (!function_exists('createConnection')) {
    function createConnection() {
-      $serverName = 'localhost';
-      $username = 'root';
-      $pw = 'Ilovephp!!99';
-      $dbName = 'composey';
+      $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      $cleardb_server = $cleardb_url["host"];
+      $cleardb_username = $cleardb_url["user"];
+      $cleardb_password = $cleardb_url["pass"];
+      $cleardb_db = substr($cleardb_url["path"],1);
+
+      // $serverName = 'localhost';
+      // $username = 'root';
+      // $pw = 'Ilovephp!!99';
+      // $dbName = 'composey';
    
-      $handler = mysqli_connect($serverName, $username, $pw, $dbName);
+      $handler = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
    
       return $handler;
    }
